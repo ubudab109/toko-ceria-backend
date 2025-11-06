15d6f8c08f1de6b825272f045ab585232b0b1e36
Muhammad Rizky Firdaus <rizky.firdaus@willowmore.com.sg>
2025-11-07 06:17:33 +0700

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
         @viteReactRefresh
-        @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+        @vite(['resources/css/app.css', 'resources/js/app.tsx'])
         @inertiaHead
         @routes
     </head>
